0dfacd5a9ccf246d324734c65a9bdc228fc4723c
sreedhar <sreedharks2003@gmail.com>
2023-04-05 01:16:30 +0530

    Created layout for details screen

--- app/details/index.js
+++ app/details/index.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import {View,Text,StyleSheet} from 'react-native';
-import { Appbar,Card,TextInput,Divider,Checkbox } from 'react-native-paper';
+import { Appbar,Card,TextInput,Divider,Button} from 'react-native-paper';
 import { useSearchParams } from 'expo-router';
+import App from '..';
 export default function Default (){
     const [checked,setChecked]=useState(false);
     const params = useSearchParams();
@@ -9,8 +10,9 @@ export default function Default (){
     return(
         <>
         <Appbar.Header>
+            <Appbar.BackAction actio />
             <Appbar.Content title={params.item_name}/>
-            <Appbar.Action icon={"logout"} />
+            
         </Appbar.Header>
         <View style={styles.container}>
            <Card style={{width:'100%',height:'auto'}}>
@@ -19,6 +21,15 @@ export default function Default (){
             </Card>
             <Text style={{fontSize:18,margin:15}}>{params.add}</Text>
             <Divider/>
+            <Text style={{fontSize:30,textAlign:'center',margin:5}}>Available foods</Text>
+            <Card>
+                <Card.Content>
+                    <Text style={{fontSize:25,marginBottom:15}}><Text style={{color:'grey'}}>Food :</Text>Briyani</Text>
+                    <Text style={{fontSize:25}}><Text style={{color:'grey'}}>Content :</Text>500 ml</Text>
+                    <Button>Request</Button>
+                </Card.Content>
+                
+            </Card>
         </View>
         </>
     );
