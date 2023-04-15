9d6e2b28f815df15b4d7868ecd6f80e618b5c319
sreedhar <sreedharks2003@gmail.com>
2023-04-15 19:14:35 +0530

    Connected firebase with Login

--- app/index.js
+++ app/index.js
@@ -6,23 +6,34 @@ import { useEffect, useState } from 'react';
 import { Card, Provider , ActivityIndicator} from 'react-native-paper';
 import { Link , useRouter} from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {getAuth} from 'firebase/auth';
+import {getAuth,signInWithEmailAndPassword} from 'firebase/auth';
 export default function App() {
   const [location,setLocation] = useState({});
   const [loading,setLoading] = useState(true);
-  const [token ,setToken] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [error,setError] = useState(false);
+  const [token ,setToken] = useState(null);
   const router = useRouter();
   const auth = getAuth();
   const getToken = async ()=>{
     const item = await AsyncStorage.getItem("token");
     try{
       if(item!=null){
-        setToken(item);
+        setToken(null);
 
       }
     }catch(err){
       console.log(err);
     }
+  }
+  const storeToken = async(token) =>{
+    try{
+      await AsyncStorage.setItem("token",token);
+    }catch(err){
+      Alert.alert(err);
+    }
+
   }
   useEffect(()=>{
     const locationRequest = async()=>{
@@ -37,15 +48,29 @@ export default function App() {
       var location = await Location.getCurrentPositionAsync({});
       setLocation(location);
       setLoading(false);
-
+  
     }
-    locationRequest();
+    if(loading==true){
+      locationRequest();
+    }
   },[loading]);
-  const login = async()=>{
-    getToken();
-    router.push({pathname:'/home',params:{lat:location["coords"]["latitude"],long:location["coords"]["longitude"]}});
+
+  const login = ()=>{
+
+    signInWithEmailAndPassword(auth,email,password)
+    .then((userCredentials) =>{
+      console.log(userCredentials.user);
+      if(userCredentials.user.uid!=null){
+        console.log(userCredentials.user.uid);
+        storeToken(userCredentials.user.uid);
+      }
+    })
+    .catch((err)=>{
+      setError(true);
+      alert(err.message);
+    })
   }
-  let userlocation = JSON.stringify(location);
+
   console.log(location);
 
   return (
@@ -54,27 +79,31 @@ export default function App() {
       <View style={styles.container}>
         <StatusBar style="auto" />
         {loading==true ? <ActivityIndicator size={24} color={'black'}/> :(
-         token=='' ? 
+         token==null ? 
            
             <>
                <Image source={require('./img/logo.png')} style={styles.logo}/>
 
  
-              <TextInput placeholder='username'  style={styles.textinput} />
-              <TextInput placeholder='password' style={styles.textinput} secureTextEntry={true}/>
+              <TextInput placeholder='username'  onChangeText={email=>setEmail(email)} style={styles.textinput} />
+              <TextInput placeholder='password'  onChangeText={password=>setPassword(password)} style={styles.textinput} secureTextEntry={true}/>
 
               <Link href={{pathname:'/home',params:{lat:location["coords"]["latitude"],long:location["coords"]["longitude"]}}} asChild>
-                 <Pressable style={styles.Pressable}><Text style={styles.Text}>LOGIN</Text></Pressable>
+                 <Pressable 
+                 onPress={()=>{
+                  login();
+                  
+                  }} style={styles.Pressable}><Text style={styles.Text}>LOGIN</Text></Pressable>
               </Link>
               <View style={styles.footer}>
                 <Text>If you are not registered, </Text>
-                <Link href='#'>
+                <Link href='/auth/register'>
                   <Text style={styles.link}>Register</Text>
                 </Link>
               </View>
             </>:(
-        router.push({pathname:'/home',params:{lat:location["coords"]["latitude"],long:location["coords"]["longitude"]}})
-      )
+              router.push({pathname:'/home',params:{lat:location["coords"]["latitude"],long:location["coords"]["longitude"]}})
+            )
        )}
       </View>
     </Provider>
