92aedd90309aae2fe4493278217d5594df3de345
Govardhan <govardhand06@gmail.com>
2023-04-15 13:46:26 +0530

    from bash

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, View } from 'react-native';
-
+import { Alert, StyleSheet, View, TextInput, Pressable, Text, Image} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
-import { Card, Provider ,TextInput, Appbar, Button, ActivityIndicator} from 'react-native-paper';
-import { Link } from 'expo-router';
+import { Card, Provider , ActivityIndicator} from 'react-native-paper';
+import { Link , useRouter} from 'expo-router';
 export default function App() {
   const [location,setLocation] = useState();
   const [loading,setLoading] = useState(true);
@@ -28,27 +28,29 @@ export default function App() {
   console.log(location);
   return (
     <Provider>
-      <Appbar.Header>
-        <Appbar.Content title="sharemeal"/>
-      </Appbar.Header>
+
       <View style={styles.container}>
         <StatusBar style="auto" />
         {loading==true ? <ActivityIndicator size={24} color={'black'}/> :(
-            <Card style={{height:'auto',width:'90%'}}>
+          
            
-            <Card.Cover source={{uri:'https://i.ibb.co/4d376Jv/materialu.png'}}/>
-           
-            <Card.Content style={{margin:5}}>
-             
-              <TextInput label="username" style={{marginBottom:30}}/>
-              <TextInput label="password"/>
-            </Card.Content>
-            <Card.Actions >
+            <>
+               <Image source={require('./img/logo.png')} style={styles.logo}/>
+
+ 
+              <TextInput placeholder='username'  style={styles.textinput} />
+              <TextInput placeholder='password' style={styles.textinput} secureTextEntry={true}/>
+
               <Link href={{pathname:'/home',params:{lat:location["coords"]["latitude"],long:location["coords"]["longitude"]}}} asChild>
-                 <Button>Login</Button>
+                 <Pressable style={styles.Pressable}><Text style={styles.Text}>LOGIN</Text></Pressable>
               </Link>
-            </Card.Actions>
-         </Card>
+              <View style={styles.footer}>
+                <Text>If you are not registered, </Text>
+                <Link href='#'>
+                  <Text style={styles.link}>Register</Text>
+                </Link>
+              </View>
+            </>
         )}
       </View>
     </Provider>
@@ -61,7 +63,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom:100
+    marginBottom:100,
+    
   },
+  textinput:{
+    backgroundColor: 'transparent',
+    margin: 0,
+    
+    marginBottom:30, backgroundColor:'#F5F8FA',borderRadius: 15, height: 50, width:'90%', padding:15, 
+
+  },
+  Pressable: {
+    backgroundColor: '#fd0136',
+    width: '90%',
+    padding:5,
+    borderRadius:20
+  },
+  Text: {
+    fontSize: 20,
+    color: 'white',
+    padding: 5,
+    textAlign: 'center'
+
+  
+  },
+  logo: {
+    width: '100%',
+    height: '25%',
+    marginBottom: 15
+  },
+  footer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 30
+  },
+  link: {
+    color: '#fd0136'
+  }
 
 });
